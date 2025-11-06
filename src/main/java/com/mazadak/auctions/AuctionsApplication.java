785a6bce278d56134a3a2b7277764c38c1c0455a
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-11-07 00:42:43 +0200

    feat: component scan for common-module

--- src/main/java/com/mazadak/auctions/AuctionsApplication.java
+++ src/main/java/com/mazadak/auctions/AuctionsApplication.java
@@ -2,6 +2,7 @@ package com.mazadak.auctions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +12,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableScheduling
+@ComponentScan(basePackages = {"com.mazadak.auctions", "com.mazadak.common"})
 public class AuctionsApplication {
 
     public static void main(String[] args) {
